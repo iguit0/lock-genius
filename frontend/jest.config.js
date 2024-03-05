422b23e507eb082abfd4e0b9a94ed5e9ce57e137
Igor Alves <igorsk89@gmail.com>
2024-03-05 15:35:26 -0300

    chore(project): add coverage option to jest

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -6,6 +6,10 @@ const createJestConfig = nextJest({
 
 const config = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**'],
+  coverageReporters: ['html'],
+  coverageDirectory: './coverage',
   testEnvironment: 'jest-environment-jsdom',
   testPathIgnorePatterns: ['<rootDir>/src/__tests__/e2e'],
 };
