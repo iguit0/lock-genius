5990206c2a2d2c3ff5f905f849dd10c1e21cfe1e
Igor Alves <igorsk89@gmail.com>
2025-08-28 13:09:59 -0300

    fix: remove duplicate import of useState in Navbar component

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,8 +1,8 @@
 'use client';
 
+import { useState } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { useState } from 'react';
 import { useSession } from 'next-auth/react';
 
 import { Icons } from '../icons';
