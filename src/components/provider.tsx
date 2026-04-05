d4bf0ca45e0dd214da916a89daa6bc902cf4b98c
Igor Alves <igorsk89@gmail.com>
2026-04-05 00:50:59 -0300

    chore: remove NextAuth files

--- src/components/provider.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import { SessionProvider } from 'next-auth/react';
-import type { ReactNode } from 'react';
-
-export const Provider = ({ children }: { children: ReactNode }) => {
-  return <SessionProvider>{children}</SessionProvider>;
-};
